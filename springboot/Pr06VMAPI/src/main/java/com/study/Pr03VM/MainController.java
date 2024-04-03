826dee82f36c30fbfa2bb47bc44376d75686ece8
nissi153 <nissisoft21@gmail.com>
2024-04-03 16:09:22 +0900

    update

--- springboot/Pr06VMAPI/src/main/java/com/study/Pr03VM/MainController.java
+++ springboot/Pr06VMAPI/src/main/java/com/study/Pr03VM/MainController.java
@@ -1,5 +1,7 @@
 package com.study.Pr03VM;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -9,7 +11,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class MainController {
+    final ProductRepository pRepo;
 
     @GetMapping("/")
     public String main(){
@@ -24,7 +28,7 @@ public class MainController {
     public String editProductForm(@RequestParam int index, Model model){
         model.addAttribute("index", index);
 
-        Product product = RestAPIController.list.get(index);
+        Product product = pRepo.list.get(index);
         model.addAttribute("product", product);
 
         return "editProductForm";
--- springboot/Pr06VMAPI/src/main/java/com/study/Pr03VM/RestAPIController.java
+++ springboot/Pr06VMAPI/src/main/java/com/study/Pr03VM/RestAPIController.java
@@ -1,5 +1,7 @@
 package com.study.Pr03VM;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -9,35 +11,39 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1")
 public class RestAPIController {
-    public static List<Product> list = new ArrayList<>();
+    //public static List<Product> list = new ArrayList<>();
+    final ProductRepository pRepo;
 
-    public RestAPIController(){
+    public RestAPIController(ProductRepository pRepo){
+        //생성자 주입
+        this.pRepo = pRepo;
+
         Product product1 = Product.builder()
                 .name("소고기버거")
                 .price(2000)
                 .limit_date(LocalDate.parse("2024-01-01")).build();
-        list.add( product1 );
+        pRepo.list.add( product1 );
         Product product2 = Product.builder()
                 .name("토마토버거")
                 .price(3000)
                 .limit_date(LocalDate.parse("2024-02-01")).build();
-        list.add( product2 );
+        pRepo.list.add( product2 );
         Product product3 = Product.builder()
                 .name("배추버거")
                 .price(4000)
                 .limit_date(LocalDate.parse("2024-03-01")).build();
-        list.add( product3 );
+        pRepo.list.add( product3 );
     }
 
     @GetMapping("/products")
     public List<Product> products(){
-        return list;
+        return pRepo.list;
     }
 
     //http://localhost:8080/deleteProduct?index=0
     @DeleteMapping("/deleteProduct")
     public ResDto deleteProduct(@RequestParam int index){
-        list.remove( index );
+        pRepo.list.remove( index );
 
         ResDto dto = ResDto.builder()
                 .status("ok").message("삭제되었습니다")
@@ -56,7 +62,7 @@ public class RestAPIController {
                 .price(dto.getInputPrice())
                 .limit_date(dto.getInputLimitDate())
                 .build();
-        list.set( index, product);
+        pRepo.list.set( index, product);
 
         ResDto resDto = ResDto.builder()
                 .status("ok").message("수정되었습니다.")
@@ -74,7 +80,7 @@ public class RestAPIController {
                 .price(dto.getInputPrice())
                 .limit_date(dto.getInputLimitDate())
                 .build();
-        list.add( product );
+        pRepo.list.add( product );
 
         ResDto resDto = ResDto.builder()
                 .status("ok").message("추가되었습니다.")
