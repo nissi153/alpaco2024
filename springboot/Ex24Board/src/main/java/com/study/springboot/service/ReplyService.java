643253ffff1003e86fa576f7b3f19912a2e57651
nissi153 <nissisoft21@gmail.com>
2024-04-16 10:30:19 +0900

    add

---
+++ springboot/Ex24Board/src/main/java/com/study/springboot/service/ReplyService.java
@@ -0,0 +1,27 @@
+package com.study.springboot.service;
+
+import com.study.springboot.domain.reply.Reply;
+import com.study.springboot.domain.reply.ReplyRepository;
+import com.study.springboot.dto.ReplySaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ReplyService {
+    final private ReplyRepository replyRepository;
+
+    //중간에 예외(오류)발생시 rollup을 수행하여, 데이터 일관성을 유지해줌.
+    @Transactional
+    public Long save(final ReplySaveRequestDto dto){
+        Reply entity = replyRepository.save( dto.toEntity() );
+        return entity.getReplyIdx();
+    }
+    @Transactional(readOnly = true)
+    public boolean existsById( Long replyIdx ){
+        boolean isFound = replyRepository.existsById(replyIdx);
+        return isFound;
+    }
+
+}
