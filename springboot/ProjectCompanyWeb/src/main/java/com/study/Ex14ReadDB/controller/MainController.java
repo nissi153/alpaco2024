c1146034f539abb53ceb08c73071531f06554000
nissi153 <nissisoft21@gmail.com>
2024-04-17 13:25:09 +0900

    update

--- springboot/ProjectCompanyWeb/src/main/java/com/study/Ex14ReadDB/controller/MainController.java
+++ springboot/ProjectCompanyWeb/src/main/java/com/study/Ex14ReadDB/controller/MainController.java
@@ -2,11 +2,38 @@ package com.study.Ex14ReadDB.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
     @GetMapping("/")
     public String main(){
-        return "first"; //first.html로 응답
+        return "index"; //index.html로 응답
+    }
+
+    @GetMapping("/member/login")
+    public String login(){
+        return "/member/login";
+    }
+
+    @GetMapping("/member/join")
+    public String join(){
+        return "/member/join2"; //join2.html 응답
+    }
+    @GetMapping("/idFind")
+    public String idFind(){
+        return "/member/idFind"; //idFind.html 응답
+    }
+    @GetMapping("/passwordFind")
+    public String passwordFind(){
+        return "/member/passwordFind"; //idFind.html 응답
+    }
+    @GetMapping("/community/community01")
+    public String community01(){
+        return "/community/community01";
+    }
+    @GetMapping("community01_1")
+    public String community01_1(@RequestParam Long no){
+        return "/community/community01_1";
     }
 }
