244eb8fb24bf4a38f9f878e01bf54ffd6572ac98
nissi153 <nissisoft21@gmail.com>
2024-04-24 13:05:16 +0900

    update

---
+++ springboot/Ex26Security02/src/main/java/com/study/springboot/MainController.java
@@ -0,0 +1,17 @@
+package com.study.springboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String main(){
+        return "index"; //index.html로 응답함
+    }
+    @GetMapping("/loginForm")
+    public String loginForm(){
+        return "loginForm"; //loginForm.html로 응답
+    }
+}
